11f1e1855eda9878138486b4dc78253e1ea76b5e
Michael Predl <michael.predl@univie.ac.at>
2023-07-17 12:10:51 +0200

    Extended the tutorial with a show-case of the analysis functions.

--- tutorial/PyCoMo_basics.py
+++ tutorial/PyCoMo_basics.py
@@ -46,7 +46,7 @@ help(pycomo)
 # 
 # In this tutorial we are using metabolic models from the AGORA collection. The models were retrieved from www.vmh.life, and are stored in the data folder of the repository. The selection of models and the resulting community represents a cystic fibrosis airway community, as done by Henson et al. (www.doi.org/10.1128/mSystems.00026-19)
 
-# In[4]:
+# In[3]:
 
 
 test_model_dir = "../data/use_case/henson"
@@ -55,7 +55,7 @@ named_models = pycomo.load_named_models_from_dir(test_model_dir)
 
 # The models and file names were extracted and stored in named_models. Let's check the contents:
 
-# In[5]:
+# In[4]:
 
 
 named_models
@@ -66,7 +66,7 @@ named_models
 
 # One of the requirements for a community metabolic model is a common biomass function. To construct it, PyCoMo requires the biomass of each member represented as a single metabolite. This biomass metabolite ID can be specified when constructing the SingleOrganismModel objects. However, it can also be found or generated automatically, by setting the biomass reaction as the objective of the model. Let's check if the biomass function is the objective in all the models
 
-# In[6]:
+# In[5]:
 
 
 for model in named_models.values():
@@ -75,7 +75,7 @@ for model in named_models.values():
 
 # With the objective being the biomass function in all models, the biomass metabolite does not need to be specified.
 
-# In[7]:
+# In[6]:
 
 
 single_org_models = []
@@ -88,7 +88,7 @@ for name, model in named_models.items():
 # ### Creating a community model ###
 # With the member models prepared, the community model can be generated. The first step is to create a CommunityModel objects from the member models. The matching of the exchange metabolites can be achieved in two ways: matching via identical metabolite IDs, or via annotation fields. In this tutorial and as all the models come from the same source, matching via identical metabolite IDs will be used.
 
-# In[8]:
+# In[7]:
 
 
 community_name = "henson_community_model"
@@ -97,7 +97,7 @@ com_model_obj = pycomo.CommunityModel(single_org_models, community_name)
 
 # The cobra model of the community will generated the first time it is needed. We can enforce this now, by calling it via .community_model
 
-# In[9]:
+# In[8]:
 
 
 com_model_obj.community_model
@@ -111,7 +111,7 @@ com_model_obj.community_model
 # ### Setting the community member composition ###
 # For the bounds of the model and the normalisation to be correct, the fractions of all community members must be set (and sum up to 1.0). A quick way to do this is to set the abundance fractions equal for all community members.
 
-# In[10]:
+# In[9]:
 
 
 com_model_obj.equal_abundance()
@@ -119,7 +119,7 @@ com_model_obj.equal_abundance()
 
 # Now let us check if the biomass function was updated accordingly as well
 
-# In[11]:
+# In[10]:
 
 
 com_model_obj.community_model.reactions.get_by_id("community_biomass").reaction
@@ -130,7 +130,7 @@ com_model_obj.community_model.reactions.get_by_id("community_biomass").reaction
 # ### Quality Checks ###
 # One of the quality checks that should be done is to look into all unbalanced reactions (mass and charge) in the entire model. As said before, such reactions should only exist in the case of boundary reactions, such as exchange, sink and source reactions.
 
-# In[12]:
+# In[11]:
 
 
 com_model_obj.get_unbalanced_reactions()
@@ -139,23 +139,79 @@ com_model_obj.get_unbalanced_reactions()
 # ## Saving and loading community models ##
 # Community model objects can be saved and loaded into SBML files. This is different from the other available option to save the cobra model of the community model objects, as the abundance fractions of the organisms are written into the file as well. Saving and loading the community model can be done like this:
 
-# In[13]:
+# In[12]:
 
 
 com_model_obj.save("../data/toy/output/henson_com_model.xml")
 
 
-# In[14]:
+# In[13]:
 
 
 com_model_obj_loaded = pycomo.CommunityModel.load("../data/toy/output/henson_com_model.xml")
 
 
-# In[15]:
+# In[14]:
 
 
 com_model_obj_loaded.community_model.summary()
 
 
 # ## Analysis of community models ##
-# Work in progress.
+# PyCoMo offers the option to calculate all potential exchange metabolites and cross-feeding interactions in a community, independent of the community composition. The example for this part will be a three member community published by Koch et al. 2019 (https://doi.org/10.1371/journal.pcbi.1006759). The three member organisms are representatives of functional guilds in a biogas community.
+# ### Creating the community model ###
+# We repeat the steps as before.
+
+# In[15]:
+
+
+test_model_dir = "../data/use_case/koch"
+named_models = pycomo.load_named_models_from_dir(test_model_dir)
+
+
+# In[16]:
+
+
+named_models
+
+
+# In[17]:
+
+
+single_org_models = []
+for name, model in named_models.items():
+    single_org_model = pycomo.SingleOrganismModel(model, name)
+    single_org_models.append(single_org_model)
+    
+community_name = "koch_community_model"
+com_model_obj = pycomo.CommunityModel(single_org_models, community_name)
+
+
+# With the community model generated, we set the medium for the analysis, as done by Koch et al.
+
+# In[18]:
+
+
+medium = {
+    'EX_CO2_EX_exchg': 1000.0,
+    'EX_SO4_EX_exchg': 1000.0,
+    'EX_H2S_EX_exchg': 1000.0,
+    'EX_Eth_EX_exchg': 1000.0,
+    'EX_Lac_EX_exchg': 1000.0,
+    'EX_Pyr_EX_exchg': 1000.0,
+    'EX_BM_tot_exchg': 1000.0
+}
+com_model_obj.medium = medium
+com_model_obj.apply_medium()
+
+
+# ### Calculating potential metabolite exchange ###
+# All potential exchange metabolite fluxes and cross-feeding interactions can be calculated with the _potential_metabolite_exchanges_ method. This is a single FVA, but with a minimum objective of 0 and relaxed constraints. All reaction constraints are changed to include the value 0, which circumvents cases where a specific flux through a reaction is required, leading to infeasible solutions for certain community compositions.
+
+# In[19]:
+
+
+com_model_obj.potential_metabolite_exchanges()
+
+
+# 
